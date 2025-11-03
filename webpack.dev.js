0a8ce7e4c4fc7c3bb934714ad8db72e186e7aebf
HagaiLevHacohen <levhacohen@mail.tau.ac.il>
2025-11-03 14:00:35 +0200

    initial finish

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,13 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: 'development',
+  mode: "development",
   devtool: "eval-source-map",
   devServer: {
     static: {
-    directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(__dirname, "dist"),
     },
     watchFiles: ["./src/template.html"],
   },
